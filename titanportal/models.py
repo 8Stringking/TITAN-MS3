2bb43e659c614cf0905dbf0165034f34c6f2c76d
8Stringking <tomcondemned@hotmail.co.uk>
2022-05-12 00:58:34 +0000

    added schema for department and colleague tables and have created the database in postgres

---
+++ titanportal/models.py
@@ -0,0 +1,27 @@
+from titanportal import db
+
+
+class Department(db.Model):
+    # schema for the department model
+    id = db.Column(db.Integer, primary_key=True)
+    department_name = db.Column(db.String(20), unique=True, nullable=False)
+    colleagues = db.relationship(
+        "Colleague", backref="department", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself inform of a string
+        return self.department_name
+
+
+class Colleague(db.Model):
+    # schema for the colleague model
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(15), unique=True, nullable=False)
+    last_name = db.Column(db.String(15), unique=True, nullable=False)
+    role = db.Column(db.String(15), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey(
+            "department.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself inform of a string
+        return f"#{self.id}-First:{self.first_name}|Last:{self.last_name}"
--- titanportal/routes.py
+++ titanportal/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from titanportal import app, db
+from titanportal.models import Department, Colleague
 
 
 @app.route("/")
