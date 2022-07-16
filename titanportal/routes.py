4577f9d7b84f74089ccc05db8775a2d72752e127
8Stringking <tomcondemned@hotmail.co.uk>
2022-07-16 13:41:32 +0000

    fixed error in routes.py to enable login required function to operate correctly

--- titanportal/routes.py
+++ titanportal/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, request, session, redirect, url_for
+from functools import wraps
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from titanportal import app, db, mongo
