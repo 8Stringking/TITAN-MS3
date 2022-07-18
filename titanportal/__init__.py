5113a98931248a7d7a1fc1e591efdbfc56ea6c65
8Stringking <tomcondemned@hotmail.co.uk>
2022-07-18 22:34:54 +0000

    attempted to update models to database however was unsuccessful, so restoring previous settings

--- titanportal/__init__.py
+++ titanportal/__init__.py
@@ -3,8 +3,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env  # noqa
-
+    import env # noqa
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -14,10 +13,9 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 uri = os.environ.get("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
 
-
-from titanportal import routes  # noqa
+from titanportal import routes # noqa
